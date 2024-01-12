1e823642b2597255e91c341972a68970f777e8fc
FathulQadriGunata <101128981+FathulQadriGunata@users.noreply.github.com>
2024-01-12 12:20:33 +0700

    Initial commit

---
+++ app/Models/NilaiFathul.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NilaiFathul extends Model
+{
+    use HasFactory;
+
+    protected $table ="nilaifathuls";
+    protected $primaryKey ="id";
+
+    public function guru(){
+        return $this->belongsTo(Guru::class, 'id_guru'); //id guru fk
+    }
+
+    public function siswa(){
+        return $this->belongsTo(Siswa::class, 'id_siswa');
+    }
+   
+    public function kurikulum(){
+        return $this->belongsTo(kurikulum::class, 'id');
+    }
+}
